ea62d8878cbb1fee1f29736820f7d19b192c96b4
Youssef <youssefyaser2020@gmail.com>
2025-03-31 11:16:00 +0200

    Handle POST and GET requests

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
+const fs = require('fs');
 const express = require('express');
-const app = express();
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello from the server side', appName: 'Natours' });
+const app = express();
+app.use(express.json());
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8'));
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
-app.post('/', (req, res) => {
-  res.send('You Can Post');
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+
+  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), (err) => {
+    if (err) {
+      console.log(err.message);
+      return;
+    }
+    res.status(201).json({ status: 'success', data: { newTour } });
+  });
 });
 
 const port = 8000;
