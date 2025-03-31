9e6a88695604e1d0b12b25a8644e1e8e5d646702
Youssef <youssefyaser2020@gmail.com>
2025-03-31 20:18:47 +0200

    Handle PATCH and DELETE requests

--- app.js
+++ app.js
@@ -41,6 +41,22 @@ app.post('/api/v1/tours', (req, res) => {
   });
 });
 
+app.patch('/api/v1/tours/:id', (req, res) => {
+  if (Number(req.params.id) > tours.length) {
+    return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
+  }
+
+  res.status(200).json({ status: 'success', data: '<element updated>' });
+});
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+  if (Number(req.params.id) > tours.length) {
+    return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
+  }
+
+  res.status(204).json({ status: 'success', data: null });
+});
+
 const port = 8000;
 app.listen(port, () => {
   console.log('Listening...');
