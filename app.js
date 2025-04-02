b8fe0467cf6686e73355cb83cb841dc6ae7f7c0f
Youssef <youssefyaser2020@gmail.com>
2025-04-02 06:49:14 +0200

    Environment Variables

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const morgan = require('morgan');
+
 const userRouter = require('./Routes/userRouter');
 const tourRouter = require('./Routes/tourRouter');
 
 const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.use(express.json());
 app.use(express.static('./public'));
