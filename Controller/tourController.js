526445fcd58e150c50892b40a0c65320a1d1ba7b
Youssef <youssefyaser2020@gmail.com>
2025-04-02 06:01:05 +0200

    Channing multiple middleware

--- Controller/tourController.js
+++ Controller/tourController.js
@@ -11,6 +11,14 @@ exports.checkID = (req, res, next, val) => {
   next();
 };
 
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price) {
+    return res.status(400).json({ status: 'fail', message: 'There is no name or price' });
+  }
+
+  next();
+};
+
 exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
@@ -37,7 +45,7 @@ exports.createTour = (req, res) => {
   const newTour = Object.assign({ id: newId }, req.body);
   tours.push(newTour);
 
-  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), (err) => {
+  fs.writeFile(`${__dirname}/../dev-data/data/tours-simple.json`, JSON.stringify(tours), (err) => {
     if (err) {
       console.log(err.message);
       return;
--- Routes/tourRouter.js
+++ Routes/tourRouter.js
@@ -2,13 +2,13 @@ const express = require('express');
 
 const tourController = require('../Controller/tourController');
 
-const { getAllTours, createTour, getTour, updateTour, deleteTour, checkID } = tourController;
+const { getAllTours, createTour, getTour, updateTour, deleteTour, checkID, checkBody } = tourController;
 
 const router = express.Router();
 
 router.param('id', checkID);
 
-router.route('/').get(getAllTours).post(createTour);
+router.route('/').get(getAllTours).post(checkBody, createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
 module.exports = router;
