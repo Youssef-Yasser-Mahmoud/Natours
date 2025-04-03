33a39db7dab2dd73e368cebbd0e66ac17679c7ec
Youssef <youssefyaser2020@gmail.com>
2025-04-03 09:31:54 +0200

    Create Tour Model and documents from that model

--- server.js
+++ server.js
@@ -8,7 +8,30 @@ const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSW
 
 mongoose.connect(DB, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false }).then((con) => {
   console.log(con);
+  console.log('Good Connection');
 });
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: { type: Number, default: 4.5 },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({ name: 'Third tour', price: 300, rating: 7 });
+
+testTour
+  .save()
+  .then((doc) => console.log(doc))
+  .catch((err) => console.log(err));
 
 const port = 8000;
 app.listen(port, () => {
