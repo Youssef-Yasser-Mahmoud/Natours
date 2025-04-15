f0b332ed4e68948cb754214141b6b5a5610cf880
Youssef <youssefyaser2020@gmail.com>
2025-04-15 22:03:55 +0200

    Added in config.env my database which is [natours]

--- server.js
+++ server.js
@@ -6,8 +6,7 @@ const app = require('./app');
 
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 
-mongoose.connect(DB, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false }).then((con) => {
-  console.log(con);
+mongoose.connect(DB, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false }).then(() => {
   console.log('Good Connection');
 });
 
